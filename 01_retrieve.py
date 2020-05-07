8f9a9d47342878c15c42687c16f5da7f0db51bb5
paaoogh <paaoogh@gmail.com>
2020-05-07 18:12:23 -0500

    changed backup route

--- 01_retrieve.py
+++ 01_retrieve.py
@@ -22,8 +22,7 @@ for event in events_list:
     filename = event.get('id') + '.json'
     with open(filename, 'w') as outfile:
         json.dump(event, outfile)
-    #with open(filename,"w") as file:
-     #   file.write(event)
+        
     output = subprocess.run(["scp",filename,"paolagh@132.247.186.67:public_html/static"])
-    output = subprocess.run(["mv",filename,"paolagh@132.247.186.67:json/backup"])
+    output = subprocess.run(["mv",filename,"json/backup/"])
 
