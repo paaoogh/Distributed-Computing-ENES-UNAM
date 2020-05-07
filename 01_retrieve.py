38c95a4eef3c58ef33f4f8bfb94fc23a8353a75d
paaoogh <paaoogh@gmail.com>
2020-05-07 18:06:55 -0500

    updating file writing

--- 01_retrieve.py
+++ 01_retrieve.py
@@ -20,8 +20,10 @@ events_list = data['events']
 print(events_list[1])
 for event in events_list:
     filename = event.get('id') + '.json'
-    with open(filename,"w") as file:
-        file.write(event)
+    with open(filename, 'w') as outfile:
+        json.dump(event, outfile)
+    #with open(filename,"w") as file:
+     #   file.write(event)
     output = subprocess.run(["scp",filename,"paolagh@132.247.186.67:public_html/static"])
     output = subprocess.run(["mv",filename,"paolagh@132.247.186.67:json/backup"])
 
