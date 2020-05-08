00630ce42712d3730a4a4828ed1e5f71bdee8df7
paaoogh <paaoogh@gmail.com>
2020-05-08 13:38:49 -0500

    updating paths

--- 01_retrieve.py
+++ 01_retrieve.py
@@ -24,5 +24,5 @@ for event in events_list:
         json.dump(event, outfile)
         
     output = subprocess.run(["scp",filename,"paolagh@132.247.186.67:public_html/static"])
-    output = subprocess.run(["mv",filename,"~/json/backup"])
+    output = subprocess.run(["mv",filename,"json/backup"])
 
