173871e6f6af8835e466167a330e2f6621937734
paaoogh <paaopaaoogh@gmail.com>
2020-08-05 22:06:15 -0500

    Order of files execution

---
+++ ordering.sh
@@ -0,0 +1,13 @@
+#!/bin/sh/env bash
+
+cd $Documents
+cd Documents
+
+direction=$(head -n 1 data.txt)
+
+while ssh $direction; do
+mkdir ~/public_html/static/hey
+exit
+done
+#mkdir -p ~/public_html/static/
+# Creating directories for each date
