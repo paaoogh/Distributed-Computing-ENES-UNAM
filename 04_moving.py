02219aba35c4a08abf2ab92c9102138704741e49
paaoogh <paaoogh@gmail.com>
2020-08-09 13:16:36 -0500

    solving for routes

--- 04_moving.py
+++ 04_moving.py
@@ -2,25 +2,26 @@
 
 import shutil
 import os
+import subprocess
 #--------*-----
 from datetime import datetime
 
 backup= "/home/paola/Documents/FILES"
 git = "/home/paola/Documents/Distributed-Computing-ENES-UNAM/"
-server = "paolagh@132.247.186.67:public_html/static"
+server = "paolagh@132.247.186.67/public_html/static/backup"
 
 dating = str(datetime.today())[0:10]
 new_folder = backup + '/' + dating
 dating_server = server + "/" + dating
 try:
-    os.mkdir(new_folder) 
-    os.mkdir(dating_server)
+    os.mkdir(new_folder)
 
 except OSError:
     print("Creation of the directory %s failed" % dating)
-else: print ("Successfully created the directory for %s" % dating)
+else: 
+    print ("Successfully created the directory for %s" % dating)
 
-for file in os.listdir(git):
+for file in os.listdir("/home/paola/Documents/"):
     filename = os.fsdecode(file)
     if filename.endswith(".json"):
-        new_path = shutil.move(git+filename, new_folder)
+        new_path = shutil.move("/home/paola/Documents/"+filename, new_folder)
--- ordering.sh
+++ ordering.sh
@@ -5,19 +5,19 @@ cd Documents
 
 #direction=$(head -n 1 data.txt)
 Environment="PATH=/home/paola/Documents/Distributed-Computing-ENES-UNAM/01_retrieve.py"
-echo Creating JSON files at EONET project ... 
+#echo Creating JSON files at EONET project ... 
 python3 Distributed-Computing-ENES-UNAM/01_retrieve.py
 
 Environment="PATH=/home/paola/Documents/Distributed-Computing-ENES-UNAM/02_dbstoring.py"
-echo Mounting queries and moving to database...
+#echo Mounting queries and moving to database...
 python3 Distributed-Computing-ENES-UNAM/02_dbstoring.py
 
 Environment="PATH=/home/paola/Documents/Distributed-Computing-ENES-UNAM/03_processing.py"
-echo Plotting data...
+#echo Plotting data...
 python3 Distributed-Computing-ENES-UNAM/03_processing.py 
 
 Environment="PATH=/home/paola/Documents/Distributed-Computing-ENES-UNAM/04_moving.py"
-echo Moving to new directory files
+#echo Moving to new directory files
 python3 Distributed-Computing-ENES-UNAM/04_moving.py 
 
 #Cron for automation ans scheduling of the tasks
