0640dafdbcc3627cb40b584f8480d12c32b64e0f
paaoogh <paaoogh@gmail.com>
2020-08-08 20:11:27 -0500

    adding more for automation

--- ordering.sh
+++ ordering.sh
@@ -19,3 +19,16 @@ python3 Distributed-Computing-ENES-UNAM/03_processing.py
 Environment="PATH=/home/paola/Documents/Distributed-Computing-ENES-UNAM/04_moving.py"
 echo Moving to new directory files
 python3 Distributed-Computing-ENES-UNAM/04_moving.py 
+
+#Cron for automation ans scheduling of the tasks
+
+#When ready, start cron with the following commands
+
+#check cron status:
+#systemctl status cron
+
+#To star it:
+#systemctl start cron
+
+#minute hour day-month month day-year user command
+#00 23 * * * paola sh /ordering.sh
